758e9fefc263bd1a38aebe9689e047c5693a38d8
Ola2024Mohammed <hawaiiict2022@gmail.com>
2024-09-15 19:15:45 +0300

    Add Images to Project

---
+++ android/app/src/main/kotlin/com/example/telentio_dashboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.telentio_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,32 @@
+class AppImages {
+  AppImages._();
+
+  static const String imagesCommunity = "assets/images/community.svg";
+
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  static const String imagesDocument = "assets/images/document.svg";
+
+  static const String imagesHelpIcon = "assets/images/helpIcon.svg";
+
+  static const String imagesJob = "assets/images/job.svg";
+
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  static const String imagesMaleOrFemale = "assets/images/maleOrFemale.svg";
+
+  static const String imagesMessage = "assets/images/message.svg";
+
+  static const String imagesNotificationIcon =
+      "assets/images/notificationIcon.svg";
+
+  static const String imagesSchedule = "assets/images/schedule.svg";
+
+  static const String imagesSearchIcon = "assets/images/searchIcon.svg";
+
+  static const String imagesSettingsIcon = "assets/images/settingsIcon.svg";
+
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  static const String imagesUpgradePicture = "assets/images/upgradePicture.svg";
+}
